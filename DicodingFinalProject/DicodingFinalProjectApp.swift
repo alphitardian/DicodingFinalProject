a69aa3f74fc9b505c751344e8b82d3f830b34b94
Ardian Pramudya Alphita <ardianpramudya81@gmail.com>
2022-10-11 13:30:51 +0700

    Initial Commit

---
+++ DicodingFinalProject/DicodingFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DicodingFinalProjectApp.swift
+//  DicodingFinalProject
+//
+//  Created by Ardian Pramudya Alphita on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DicodingFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
